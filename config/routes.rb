882656d57fed7e8a2ebaf508e050963bd7d36dce
Carolyn Huynh <ckhuynh20@gmail.com>
2014-09-15 21:13:44 -0700

    Create UI for log in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root :to => 'sessions#new'
+
   resources :sessions, :except => [:update, :show, :index]
+  
   resources :users do
     resources :lines do
       resources :votes
